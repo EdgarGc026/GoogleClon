362eb6da4b82570776f8032a07de8940c6cc7f61
edgargc026 <edgargc_26@hotmail.com>
2021-10-02 23:53:23 -0500

    Create google clone Vue version finished

---
+++ src/components/PxLogo.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="logo-section">
+    <img :src="logoImg" :alt="logoName" />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "px-logo",
+  props: {
+    logoImg: {
+      type: String,
+      required: true,
+    },
+    logoName: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.logo-section {
+  width: 530px;
+  margin: 0 auto;
+  margin-bottom: 35px;
+}
+
+.logo-section img {
+  width: 300px;
+}
+</style>
